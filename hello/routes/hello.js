2fd11e8f734a4f1d439c1a285ab221c1cec037a9
Angela Xu <a1xu@ucsd.edu>
2019-01-24 10:28:04 -0800

    hello world app

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,14 @@
  */
 
 exports.view = function(req, res){
+  if( req.params.userName == undefined ){
+    var nameToShow = "World";
+  }
+  else {
+    var nameToShow = req.params.userName;
+  }
+
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
