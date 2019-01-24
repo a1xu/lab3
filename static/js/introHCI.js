d6c65a8e5abfc7fc987e62637a32c6ddb41d8940
Angela Xu <a1xu@ucsd.edu>
2019-01-24 09:59:22 -0800

    added interactivity

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,29 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Experience Chan! It's not a lot of questions.");
+		// $(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	// $(this).css("background-color", "#6E96D1");
+
+	// var containingProject = $(this).closest(".project");
+    // containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle();
+    }
 }
